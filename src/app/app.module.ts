0af30608ef5be0337bd625ddf91191b52c64ab9c
Henry Carvalho <47277476+HenryCL@users.noreply.github.com>
2023-02-24 14:58:03 -0300

    New Components / add app-router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SectionComponent } from './components/section/section.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    NavBarComponent
+    NavBarComponent,
+    SectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,AppRoutingModule,FormsModule],
