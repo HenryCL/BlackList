3d8728735ed74cfdc0605a341118d6d10df314ea
Henry Carvalho <47277476+HenryCL@users.noreply.github.com>
2023-03-01 14:21:53 -0300

    feat(photos): adicionado módulo com interface para imagens, criada lista de imagens no component photos e atualizado component HTML com NgFor

---
+++ src/app/components/photos/imagem.model.ts
@@ -0,0 +1,6 @@
+export interface Imagem {
+    id: number;
+    title: string;
+    url: string;
+  }
+  
--- src/app/components/photos/photos.component.ts
+++ src/app/components/photos/photos.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Imagem } from './imagem.model';
 
 @Component({
-  selector: 'app-photos',
+  selector: 'app-imagem-lista',
   templateUrl: './photos.component.html',
   styleUrls: ['./photos.component.css']
 })
-export class PhotosComponent {
 
+export class PhotosComponent {
+  imagens: Imagem[] = [
+    { id: 1, title: 'Imagem 1', url: 'assets/black.jpg' },
+    { id: 2, title: 'Imagem 2', url: 'assets/black.jpg' },
+    { id: 3, title: 'Imagem 3', url: 'assets/black.jpg' },
+    { id: 4, title: 'Imagem 4', url: 'assets/black.jpg' },
+    { id: 5, title: 'Imagem 5', url: 'assets/black.jpg' },
+    { id: 6, title: 'Imagem 6', url: 'assets/black.jpg' },
+    
+  ];
 }
+
+
+
